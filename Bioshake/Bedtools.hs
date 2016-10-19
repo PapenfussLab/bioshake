97c86c175ae4e41df4b58a39d5c3e62836691822
Justin Bedo <cu@cua0.org>
2016-10-20 10:19:36 +1100

    fixup bedtools conversion

--- Bioshake/Bedtools.hs
+++ Bioshake/Bedtools.hs
@@ -4,6 +4,7 @@ module Bioshake.Bedtools(convert) where
 import Bioshake
 import Bioshake.Internal.Bedtools
 import Development.Shake
+import Development.Shake.FilePath
 
 instance IsBam a => Buildable a (Convert "bam" "bed") where
   build params (paths -> [input]) [out] =
@@ -13,12 +14,14 @@ instance (IsPairedEnd a, IsBam a) => Buildable a (Convert "bam" "bedpe") where
   build params (paths -> [input]) [out] =
     cmd "bedtools bamtobed" ["-i", input] "-bedpe" (FileStdout out)
 
-instance (Implicit_ FilePath, IsBed a) => Buildable a (Convert "bed" "bam") where build = bedtobam
-instance (Implicit_ FilePath, IsVCF a) => Buildable a (Convert "vcf" "bam") where build = bedtobam
-instance (Implicit_ FilePath, IsGff a) => Buildable a (Convert "gff" "bam") where build = bedtobam
+instance (Referenced a, IsBed a) => Buildable a (Convert "bed" "bam") where build = bedtobam
+instance (Referenced a, IsVCF a) => Buildable a (Convert "vcf" "bam") where build = bedtobam
+instance (Referenced a, IsGff a) => Buildable a (Convert "gff" "bam") where build = bedtobam
 
-bedtobam params (paths -> [input]) [out] =
-  cmd "bedtools bedtobam" ["-i", input] ["-g", param_] (FileStdout out)
+bedtobam params a@(paths -> [input]) [out] = do
+  let genome = getRef a -<.> "genome"
+  need [genome]
+  cmd "bedtools bedtobam" ["-i", input] ["-g", genome] (FileStdout out)
 
 instance IsBed a => Buildable a (Intersect "bed") where
   build params (paths -> [input1, input2]) [out] =
