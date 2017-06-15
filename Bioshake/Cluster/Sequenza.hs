7eb27c814e691ab52738620bf2ca0a540361214d
Justin Bedo <cu@cua0.org>
2017-06-15 10:19:55 +1000

    Revert sequenza changes

--- Bioshake/Cluster/Sequenza.hs
+++ Bioshake/Cluster/Sequenza.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell       #-}
-module Bioshake.Cluster.Sequenza (pileup2Seqz, GC(..)) where
+module Bioshake.Cluster.Sequenza (pileup2Seqz, bin, GC(..)) where
 
 import           Bioshake
 import           Bioshake.Cluster.Torque
@@ -13,3 +13,4 @@ import           Development.Shake
 import           Development.Shake.FilePath
 
 $(makeSingleCluster ''Pileup2Seqz [''IsMPileup, ''GC] 'buildPileup2Seqz)
+$(makeSingleCluster ''Bin [''IsSeqzGZ] 'buildBin)
--- Bioshake/Sequenza.hs
+++ Bioshake/Sequenza.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell       #-}
-module Bioshake.Sequenza (pileup2Seqz, GC(..)) where
+module Bioshake.Sequenza (pileup2Seqz, bin, GC(..)) where
 
 import           Bioshake
 import           Bioshake.Internal.Sequenza
@@ -11,3 +11,4 @@ import           Development.Shake
 import           Development.Shake.FilePath
 
 $(makeSingleThread ''Pileup2Seqz [''IsMPileup, ''GC] 'buildPileup2Seqz)
+$(makeSingleThread ''Bin [''IsSeqzGZ] 'buildBin)
